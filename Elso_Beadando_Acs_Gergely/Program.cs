521904e56502c4b45a33b19ef1b8e58a34ab71a3
valami <acs.gergely9@gmail.com>
2019-09-27 10:47:12 +0200

    Negyedik feladat

--- Elso_Beadando_Acs_Gergely/Program.cs
+++ Elso_Beadando_Acs_Gergely/Program.cs
@@ -4,12 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Elso_Beadando_Acs_Gergely
+namespace gyakorlas
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                List<int> lista = new List<int>();
+                int bekertSzam = 0;
+                int max = 0;
+                int min = 10000;
+                int bekeresSzama = 0;
+                Console.WriteLine("Kérem adja meg hány szám közül szeretné megkeresni a legnagybbat: ");
+                bekeresSzama = Convert.ToInt16(Console.ReadLine());
+
+                for (int i = 0; i < bekeresSzama; i++)
+                {
+                    Console.WriteLine("Kérem, adjon meg egy számot.");
+                    bekertSzam = Convert.ToInt16(Console.ReadLine());
+                    lista.Add(bekertSzam);
+
+                }
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    if (max < lista[i])
+                    {
+                        max = lista[i];
+                    }
+
+                }
+                Console.WriteLine("A legnagyobb szám:{0} ", max);
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    if (min > lista[i])
+                    {
+                        min = lista[i];
+                    }
+
+                }
+                Console.WriteLine("A legkisebb szám: {0}", min);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Kérem csak számot használjon."
+                    );
+            }
+
+            Console.ReadKey();
         }
+
     }
 }
